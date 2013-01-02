56dd370e9f509787795183af2aa02d1696f20555
Tobias Sturm <=>
2013-01-02 21:58:18 +0100

    Message holds senders InetAddress

--- Buschtrommel/src/de/tr0llhoehle/buschtrommel/models/Message.java
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/models/Message.java
@@ -3,6 +3,8 @@
  */
 package de.tr0llhoehle.buschtrommel.models;
 
+import java.net.InetAddress;
+
 /**
  * @author tobi
  * 
@@ -12,8 +14,21 @@ public abstract class Message {
 	public static char MESSAGE_SPERATOR = 30;
 
 	protected String type;
+	protected InetAddress source;
 
 	public abstract String Serialize();
+	
+	/**
+	 * Indicates the sender of the message, or null if the message was created on this system
+	 * @return sender or null
+	 */
+	public InetAddress getSource() {
+		return source;
+	}
+	
+	public void setSource(InetAddress s) {
+		source = s;
+	}
 	
 	@Override
 	public String toString() {
