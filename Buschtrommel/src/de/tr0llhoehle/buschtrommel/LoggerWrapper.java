2c8e771992e700c6bb40414572a06649f5309dba
Tobias Sturm <=>
2013-01-02 18:17:09 +0100

    Add LoggerWrapper
    for convenient use of java logger api

---
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/LoggerWrapper.java
@@ -0,0 +1,32 @@
+package de.tr0llhoehle.buschtrommel;
+
+import java.util.logging.Logger;
+
+/**
+ * Wrapper for the java logging utils.
+ * 
+ * Provides a single logger for the complete application. Use methods logInfo and logError for convenient use.
+ * 
+ * @author tobi
+ *
+ */
+public class LoggerWrapper {
+	
+	public final static Logger LOGGER = Logger.getLogger("buschtrommel");
+	
+	/**
+	 * Writes info message into the logger
+	 * @param message
+	 */
+	public final static void logInfo(String message) {
+		LOGGER.info(message);
+	}
+	
+	/**
+	 * Writes warning message into the logger.
+	 * @param message
+	 */
+	public final static void logError(String message) {
+		LOGGER.warning(message);
+	}
+}
