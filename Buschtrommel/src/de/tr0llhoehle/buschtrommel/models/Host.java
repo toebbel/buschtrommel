cff98cef0cb3e75f1ae11b54791b26c8c8f37ddd
Tobias Sturm <=>
2013-01-02 20:29:39 +0100

    implement Share and Host Model

--- Buschtrommel/src/de/tr0llhoehle/buschtrommel/models/Host.java
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/models/Host.java
@@ -1,5 +1,92 @@
 package de.tr0llhoehle.buschtrommel.models;
 
-public class Host {
+import java.util.Date;
+import java.util.Hashtable;
 
+import de.tr0llhoehle.buschtrommel.LoggerWrapper;
+
+public class Host {
+	private Date lastSeen;
+	private Date firstSeen;
+	private java.net.InetAddress address;
+	private String displayName;
+	private Hashtable<String, File> shares;
+	
+	/**
+	 * Creates an instance of Host with an emtpy share
+	 * @param address address of the host (IPv4 / IPv6)
+	 * @param displayName human readable Displayname. Can be empty (IP address will be used). Message and Field seperator will be replaced with spaces.
+	 */
+	public Host(java.net.InetAddress address, String displayName) {
+		firstSeen = new Date();
+		lastSeen = firstSeen;
+		this.address = address;
+		this.displayName = displayName.replace(Message.FIELD_SEPERATOR, ' ').replace(Message.MESSAGE_SPERATOR, ' ');
+		if(this.displayName.trim().equals(""))
+			this.displayName = address.toString();
+		shares = new Hashtable<>();
+	}
+	
+	public Date getLastSeen() {
+		return lastSeen;
+	}
+	
+	public Date getFirstSeen() {
+		return firstSeen;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
+	
+	public java.net.InetAddress getAddress() {
+		return address;
+	}
+	
+	/**
+	 * Returns a *clone* of the share-table
+	 * @return clone.
+	 */
+	@SuppressWarnings("unchecked")
+	public Hashtable<String, File> getSharedFiles() {
+		return (Hashtable<String, File>) shares.clone();
+	}
+	
+	/**
+	 * Adds or replaces a offered file of this host
+	 * @param f the file to add
+	 */
+	public void addFileToSharedFiles(File f) {
+		LoggerWrapper.logInfo("Add file '" + f + "' to " + this.toString());
+		shares.put(f.getHash(), f);
+	}
+	
+	/**
+	 * Removes a file from the currently offered files of this host.
+	 * @param hash hash of the file in upper-case and without leading 0x.
+	 */
+	public void removeFileFromSharedFiles(String hash) {
+		LoggerWrapper.logInfo("Remove file with hash '" + hash + "' from " + this.toString());
+		shares.remove(hash);
+	}
+	
+	/**
+	 * Updates the LastSeen field to current time
+	 */
+	public void Seen() {
+		LoggerWrapper.logInfo("Seen host " + this.toString());
+		lastSeen = new Date();
+	}
+	
+	@Override
+	public String toString() {
+		return "Host " + address.toString() + " (" + displayName + ")";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Host))
+			return false;
+		return ((Host) obj).getAddress().equals(address);
+	}
 }
---
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/models/Share.java
@@ -0,0 +1,12 @@
+package de.tr0llhoehle.buschtrommel.models;
+
+public class Share extends File {
+
+	private String path;
+	
+	public Share(String hash, long length, int ttl, String displayName, String meta, String path) {
+		super(hash, length, ttl, displayName, meta);
+		this.path = path;
+	}
+
+}
