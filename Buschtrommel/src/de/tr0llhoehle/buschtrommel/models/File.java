c89a01fe65f4376d9e7b8c04492398108d5753a0
Tobias Sturm <=>
2013-01-02 17:29:48 +0100

    finish file model

--- Buschtrommel/src/de/tr0llhoehle/buschtrommel/models/File.java
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/models/File.java
@@ -1,36 +1,67 @@
 package de.tr0llhoehle.buschtrommel.models;
 
 public class File {
-	
+
+	public static int TTL_INFINITY = -1;
+
 	private String hash;
 	private long length;
 	private int ttl;
 	private String displayName;
 	private String meta;
-	
-	
-	
-	private String getHash() {
+
+	/**
+	 * Creates an instance of file that is eigher local or on a remote client
+	 * 
+	 * @param hash
+	 *            the hash of the file as uppercase String, without leading 0x
+	 * @param length
+	 *            number of bytes in the file
+	 * @param ttl
+	 *            TimeToLive in seconds.
+	 * @param displayName
+	 *            human readable string that represents the filename
+	 * @param meta
+	 *            any information about the file (description or path)
+	 */
+	public File(String hash, long length, int ttl, String displayName,
+			String meta) {
+		this.hash = hash;
+		this.length = length;
+		this.ttl = ttl;
+		this.displayName = displayName;
+		this.meta = meta;
+	}
+
+	public String getHash() {
 		return hash;
 	}
-	
-	private long getLength() {
+
+	public long getLength() {
 		return length;
 	}
 
-	private int getTTL() {
+	public int getTTL() {
 		return ttl;
 	}
-	
-	private void setTTL(int ttl) {
+
+	/**
+	 * Sets the TTL to given value. Must be >= 0 or Infinity
+	 * 
+	 * @param ttl
+	 *            new ttl
+	 */
+	public void setTTL(int ttl) {
+		if (!(ttl >= 0 || ttl == TTL_INFINITY))
+			throw new IllegalArgumentException("TTL is invalid (" + ttl + ")");
 		this.ttl = ttl;
 	}
-	
-	private String getDisplayName() {
+
+	public String getDisplayName() {
 		return displayName;
 	}
-	
-	private String getMeta() {
+
+	public String getMeta() {
 		return meta;
 	}
 }
