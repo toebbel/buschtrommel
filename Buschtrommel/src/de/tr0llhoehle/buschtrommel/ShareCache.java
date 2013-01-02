0bc3a958ee0f1326497148ce4a9cc536ad3b91c5
Benjamin <kontakt@benjamin-halbrock.de>
2013-01-02 23:09:29 +0100

    Sollte gehen, schreibe gerade die Tests

--- Buschtrommel/src/de/tr0llhoehle/buschtrommel/ShareCache.java
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/ShareCache.java
@@ -1,9 +1,16 @@
 package de.tr0llhoehle.buschtrommel;
 
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
 
+import de.tr0llhoehle.buschtrommel.models.FileAnnouncementMessage;
 import de.tr0llhoehle.buschtrommel.models.Share;
 
 /**
@@ -47,30 +54,71 @@ public class ShareCache {
 		return null;
 
 	}
-	
-	public String getAllShares(){
+
+	/**
+	 * creates a FileAnnouncement message with all shares
+	 * 
+	 * @return a FileAnnouncement, when there are no Shares a empty String is
+	 *         returned
+	 */
+	public String getAllShares() {
 		StringBuilder allShares = new StringBuilder("");
-		for (Share share : shares.elements()){
-			
+		Enumeration<Share> e = shares.elements();
+		while (e.hasMoreElements()) {
+			allShares.append(new FileAnnouncementMessage(e.nextElement()).Serialize());
 		}
-		
-		
-		return allShares;
+
+		return allShares.toString();
 	}
 
 	/**
 	 * 
 	 * @param share
 	 */
-	public void newShare(Share share){
-		if(share == null){
+	public void newShare(Share share) {
+		if (share == null) {
 			return;
 		}
-		String hash = share.getHash();
-		if(this.has(share.getHash())){
-			this.shares.remove(key)
+		// String hash = share.getHash();
+		if (this.has(share.getHash())) {
+			LoggerWrapper.logError("A Share with the given Hash: " + share.getHash()
+					+ "has already be defined. It is now replaced with the new Share");
+			this.shares.remove(share.getHash());
+			this.shares.put(share.getHash(), share);
 		}
-		
-		
+
+	}
+
+	public void saveToFile(String path) {
+		if (path == null || !path.endsWith(".xml")) {
+			LoggerWrapper.logError("the given path: " + path + " is not valid (must end with .xml)");
+		}
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(path);
+			XMLEncoder e = new XMLEncoder(fos);
+			e.writeObject(shares);
+			e.close();
+		} catch (FileNotFoundException e1) {
+			LoggerWrapper.logError("the given path: " + path + " is not valid");
+
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public void restoreFromFile(String path) {
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(path);
+			XMLDecoder r = new XMLDecoder(fis);
+			Hashtable<String, Share> temp_shares;
+			temp_shares = ((Hashtable<String, Share>) r.readObject());
+			r.close();
+			shares = temp_shares;
+		} catch (FileNotFoundException e) {
+			LoggerWrapper.logError("File not found: " + path);
+		}
+
 	}
 }
