432a25f71182296d9adaadf4e62a1819870e19b6
Moritz Winter <moritz.winter@gmail.com>
2013-01-02 23:57:55 +0100

    modified NetCache.java

--- Buschtrommel/src/de/tr0llhoehle/buschtrommel/network/NetCache.java
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/network/NetCache.java
@@ -19,6 +19,7 @@ public class NetCache implements IMessageObserver {
 	public void receiveMessage(Message message) {
 		if(message instanceof FileAnnouncementMessage) {
 			String hash = ((FileAnnouncementMessage) message).getFile().getHash();
+			File file = ((FileAnnouncementMessage) message).getFile();
 			int ttl = ((FileAnnouncementMessage) message).getFile().getTTL();
 			Host host = new Host(message.getSource(), message.getSource().toString());
 			if(this.knownShares.containsKey(hash)) {
@@ -35,10 +36,28 @@ public class NetCache implements IMessageObserver {
 					}
 					if(!found) {
 						//TODO: add additional information to host
+						host.addFileToSharedFiles(file);
 						this.knownHosts.put(host.getAddress(), host);
 					}
 					this.knownShares.get(hash).addHost(host);
 				}
+			} else {
+				boolean found = false;
+				for(InetAddress address : this.knownHosts.keySet()) {
+					if(host.equals(this.knownHosts.get(address))) {
+						host = this.knownHosts.get(address);
+						file.addHost(host);
+						found = true;
+						break;
+					}
+				}
+				//TODO: add additional information to host
+				host.addFileToSharedFiles(file);
+				if(!found) {
+					this.knownHosts.put(host.getAddress(), host);
+				}
+				
+				this.knownShares.put(hash, file);
 			}
 		} else if(message instanceof PeerDiscoveryMessage) {
 			
