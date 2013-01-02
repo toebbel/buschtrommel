34afb31b1587a1e1d6eb0bac15f2533bd1f4af11
Tobias Sturm <=>
2013-01-02 19:11:31 +0100

    add toString and equals to Message model

--- Buschtrommel/src/de/tr0llhoehle/buschtrommel/models/Message.java
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/models/Message.java
@@ -14,4 +14,16 @@ public abstract class Message {
 	protected String type;
 
 	public abstract String Serialize();
+	
+	@Override
+	public String toString() {
+		return Serialize();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Message))
+			return false;
+		return ((Message) obj).Serialize().equals(Serialize());
+	}
 }
