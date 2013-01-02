51922b7d098b39f8fede8a48dc04c7b05af8ac84
Moritz Winter <moritz.winter@gmail.com>
2013-01-02 17:36:58 +0100

    added interface functions

--- Buschtrommel/src/de/tr0llhoehle/buschtrommel/IGUICallbacks.java
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/IGUICallbacks.java
@@ -1,5 +1,12 @@
 package de.tr0llhoehle.buschtrommel;
 
-public interface IGUICallbacks {
+import de.tr0llhoehle.buschtrommel.models.File;
+import de.tr0llhoehle.buschtrommel.models.Host;
 
+public interface IGUICallbacks {
+	public void NewHostDiscovered(Host host);
+	public void HostWentOffline(Host host);
+	public void NewFileAvailable(File file);
+	public void FileTransferComplete(ITransferProgress transferProgress);
+	public void FileTransferFailed(ITransferProgress transferProgress);
 }
