4c7db422a8b4e492597ae1cd200d75f39db1f655
Tobias Sturm <tobstu@gmail.com>
2013-01-10 21:22:29 +0100

    (Re)Implement ShareCache save/load
    closes #19

--- Buschtrommel/test/de/tr0llhoehle/buschtrommel/test/TestShareCache.java
+++ Buschtrommel/test/de/tr0llhoehle/buschtrommel/test/TestShareCache.java
@@ -2,12 +2,15 @@ package de.tr0llhoehle.buschtrommel.test;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import de.tr0llhoehle.buschtrommel.ShareCache;
 import de.tr0llhoehle.buschtrommel.models.LocalShare;
+import de.tr0llhoehle.buschtrommel.models.Share;
 
-public class ShareCacheTest {
+public class TestShareCache {
 	@Test
 	public void testAddShare() {
 		ShareCache troll = new ShareCache();
@@ -25,24 +28,27 @@ public class ShareCacheTest {
 	}
 
 	@Test
-	public void testStore() {
-		ShareCache troll = new ShareCache();
-		LocalShare share = new LocalShare("AC3", 29, 31, "test blubb", "meta", "/home/path");
-
-		LocalShare share2 = new LocalShare("asdasd", 29, 31, "test blubb", "meta", "/home/path");
+	public void testSaveAndLoad() throws IOException {
+		(new java.io.File("test.file")).createNewFile();
+		ShareCache shares = new ShareCache();
+		LocalShare s1 = new LocalShare("ABC", 1044, Share.TTL_INFINITY, "file 1", "metablub", "test.file");
+		LocalShare s2 = new LocalShare("asdasdasdasdsa", 10, 100, "file 2", "", "test2.file");
+		shares.newShare(s1);
+		shares.newShare(s2);
+		shares.saveToFile("test.ht");
 		
-		troll.newShare(share);
-		troll.newShare(share2);
-
-		assertTrue(troll.has(share.getHash()));
-
-		troll.saveToFile("shares.ht");
-
-		ShareCache troll2 = new ShareCache();
-
-		troll2.restoreFromFile("shares.ht");
-
-		assertTrue(troll2.has(share.getHash()));
+		ShareCache loaded = new ShareCache();
+		loaded.restoreFromFile("test.ht");
+		assertTrue(loaded.has("ABC"));
+		assertEquals(loaded.get("ABC").getDisplayName(), s1.getDisplayName());
+		assertEquals(loaded.get("ABC").getHash(), s1.getHash());
+		assertEquals(loaded.get("ABC").getLength(), s1.getLength());
+		assertEquals(loaded.get("ABC").getMeta(), s1.getMeta());
+		assertEquals(loaded.get("ABC").getTTL(), s1.getTTL());
+		assertEquals(loaded.get("ABC").getPath(), (new java.io.File(s1.getPath()).getAbsolutePath()));
+		assertFalse(loaded.has("asdasdasdasdsa"));
+		(new java.io.File("test.file")).delete();
+		(new java.io.File("test.ht")).delete();
 
 	}
 
