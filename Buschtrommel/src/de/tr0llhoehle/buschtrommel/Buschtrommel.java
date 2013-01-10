122a3664ab005f88f51ca8488dd3af8461d3e014
Tobias Sturm <tobstu@gmail.com>
2013-01-10 22:29:51 +0100

    implement addShare-method

--- Buschtrommel/src/de/tr0llhoehle/buschtrommel/Buschtrommel.java
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/Buschtrommel.java
@@ -1,14 +1,17 @@
 package de.tr0llhoehle.buschtrommel;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Hashtable;
 
 import de.tr0llhoehle.buschtrommel.models.ByeMessage;
+import de.tr0llhoehle.buschtrommel.models.FileAnnouncementMessage;
 import de.tr0llhoehle.buschtrommel.models.GetFileMessage;
 import de.tr0llhoehle.buschtrommel.models.LocalShare;
 import de.tr0llhoehle.buschtrommel.models.RemoteShare;
 import de.tr0llhoehle.buschtrommel.models.Host;
+import de.tr0llhoehle.buschtrommel.models.Share;
 import de.tr0llhoehle.buschtrommel.network.FileTransferAdapter;
 import de.tr0llhoehle.buschtrommel.network.ITransferProgress;
 import de.tr0llhoehle.buschtrommel.network.IncomingDownload;
@@ -21,13 +24,15 @@ public class Buschtrommel {
 	private FileTransferAdapter fileTransferAdapter;
 	private UDPAdapter udpAdapter;
 	private NetCache netCache;
+	private ShareCache shareCache;
 
 	public Buschtrommel(IGUICallbacks gui) {
 		if (!HashFuncWrapper.check()) {
 			//cancel bootstrap: Hashfunction is not available!
-			this.gui = gui;
-			this.netCache = new NetCache(this.udpAdapter);
 		}
+		this.gui = gui;
+		this.netCache = new NetCache(this.udpAdapter);
+		this.shareCache = new ShareCache();
 	}
 	
 	public void start() throws IOException {
@@ -72,8 +77,39 @@ public class Buschtrommel {
 		return fileTransferAdapter.DownloadFile(hash, s.getHostList(), s.getLength(), new java.io.File(targetFile));
 	}
 	
-	public void AddFileToShare(String path, String dspName, String meta) {
+	/**
+	 * Creates a local share with the default TTL an announces it via multicast.
+	 * @param path path to file to share
+	 * @param dspName display name 
+	 * @param meta meta information about the file
+	 * @throws IOException when there was an error, generating the hash
+	 * @throws IllegalArgumentException when the path is not a filepath or non-existent or when the TTL is invalid
+	 */
+	public void AddFileToShare(String path, String dspName, String meta) throws IOException, IllegalArgumentException {
+		AddFileToShare(path, dspName, meta, Config.defaultTTL);
+	}
+	
+	/**
+	 * Creates a share and announces it via multicast
+	 * @param path path to the file to share
+	 * @param dspName display name
+	 * @param meta meta information about the file
+	 * @param ttl the time to live in seconds. -1 for infinity
+	 * @throws IllegalArgumentException when the path is not a filepath or non-existent or when the TTL is invalid
+	 */
+	public void AddFileToShare(String path, String dspName, String meta, int ttl) throws IOException, IllegalArgumentException {
+		File file = new File(path);
+		if(!file.exists())
+			throw new IllegalArgumentException("path does not exist!");
+		if(!file.isFile())
+			throw new IllegalArgumentException("path is not a file!");
+		if(ttl < 0 && ttl != Share.TTL_INFINITY)
+			throw new IllegalArgumentException("TTL is invalid");
 		
+		String hash = HashFuncWrapper.hash(path);
+		LocalShare share = new LocalShare(hash, file.length(), ttl, dspName, meta, path);
+		shareCache.newShare(share);
+		udpAdapter.sendMulticast(new FileAnnouncementMessage(share));
 	}
 	
 	public void RemoveFileFromShare(LocalShare file) {
