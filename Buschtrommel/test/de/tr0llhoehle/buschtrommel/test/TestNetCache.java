841034d48cbc02b406b9661420e544c73295a0a5
Moritz Winter <moritz.winter@gmail.com>
2013-01-18 03:17:15 +0100

    fixed testcases

--- Buschtrommel/test/de/tr0llhoehle/buschtrommel/test/TestNetCache.java
+++ Buschtrommel/test/de/tr0llhoehle/buschtrommel/test/TestNetCache.java
@@ -72,11 +72,27 @@ public class TestNetCache {
 
 	@Test
 	public void testTTLExpiration() throws UnknownHostException, InterruptedException {
-		NetCache tmp = new NetCache(null, null, null);
-		FileAnnouncementMessage message = new FileAnnouncementMessage(new LocalShare("testhash", 42, 10, "katze",
-				"katzenbilder!!!", "/home/katze.jpg"));
+NetCache tmp = new NetCache(null, null, null);
+		
+		PeerDiscoveryMessage peermessage = new PeerDiscoveryMessage(PeerDiscoveryMessage.DiscoveryMessageType.HI, "troll",
+				1234);
+		peermessage.setSource(new InetSocketAddress(InetAddress.getByName("localhost"), 4747));
+		
+		Host host = new Host(InetAddress.getByName("localhost"), "troll", 1234);
+		
+		tmp.receiveMessage(peermessage);
+		
+		Thread.currentThread().sleep(100);
+		
+		LocalShare share = new LocalShare("testhash", 42, 10, "katze", "katzenbilder!!!", "/home/katze.jpg");
+		
+		FileAnnouncementMessage message = new FileAnnouncementMessage(share);
+		
 		message.setSource(new InetSocketAddress(InetAddress.getByName("localhost"), 4747));
+		
 		tmp.receiveMessage(message);
+		
+		Thread.currentThread().sleep(100);
 
 		assertTrue(tmp.shareExists("testhash"));
 
