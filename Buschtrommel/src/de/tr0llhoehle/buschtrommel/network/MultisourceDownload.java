b318e9deb35f5e5cdf904502650e4701aae2ccc9
Tobias Sturm <tobstu@gmail.com>
2013-01-31 23:43:26 +0100

    implement assembleParts and checkIntegrity

--- Buschtrommel/src/de/tr0llhoehle/buschtrommel/network/MultisourceDownload.java
+++ Buschtrommel/src/de/tr0llhoehle/buschtrommel/network/MultisourceDownload.java
@@ -1,6 +1,9 @@
 package de.tr0llhoehle.buschtrommel.network;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
@@ -8,8 +11,7 @@ import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.net.ssl.SSLEngineResult.Status;
-
+import de.tr0llhoehle.buschtrommel.HashFuncWrapper;
 import de.tr0llhoehle.buschtrommel.models.GetFileMessage;
 import de.tr0llhoehle.buschtrommel.models.Host;
 
@@ -20,9 +22,12 @@ public class MultisourceDownload extends Transfer {
 	IHostPortResolver hostResolver;
 	java.io.File targetFile;
 	HashMap<InetAddress, Host> hosts;
+	boolean checkIntegrity;
 	Timer watchDog;
-	
-	public MultisourceDownload(ArrayList<InetSocketAddress> partners, GetFileMessage file, java.io.File targetFile, IHostPortResolver hostResolver) {
+
+	public MultisourceDownload(ArrayList<InetSocketAddress> partners,
+			GetFileMessage file, java.io.File targetFile,
+			IHostPortResolver hostResolver) {
 		super(null);
 		assert partners.size() > 0;
 		this.hash = file.getHash();
@@ -30,39 +35,48 @@ public class MultisourceDownload extends Transfer {
 		this.targetFile = targetFile;
 		hosts = new HashMap<>();
 		watchDog = new Timer();
-		
-		//check if ports for all hosts are known
-		for(InetSocketAddress i : partners) {
+		checkIntegrity = true;
+		logger = java.util.logging.Logger.getLogger("incoming multi " + hash);
+
+		// check if ports for all hosts are known
+		for (InetSocketAddress i : partners) {
 			Host tmp = hostResolver.getOrCreateHost(i.getAddress());
-			if(tmp.getPort() != Host.UNKNOWN_PORT)
+			if (tmp.getPort() != Host.UNKNOWN_PORT)
 				hosts.put(i.getAddress(), tmp);
 		}
-		
-		//spawn subThreads
-		subTransfers = spawnDownloads(hosts.size(), hash, file.getOffset(), file.getLength(), new ArrayList<Host>(hosts.values()), hostResolver);
+
+		// spawn subThreads
+		subTransfers = spawnDownloads(hosts.size(), hash, file.getOffset(),
+				file.getLength(), new ArrayList<Host>(hosts.values()),
+				hostResolver);
 		transferState = TransferStatus.Initialized;
 	}
-	
-	private static IncomingDownload[] spawnDownloads(int num, String hash, long start_offset, long total_length, ArrayList<Host> hosts, IHostPortResolver hostResolver) {
-		IncomingDownload[] subTransfers = new IncomingDownload[Math.min(MAX_DOWNLOADS, num)];
+
+	private static IncomingDownload[] spawnDownloads(int num, String hash,
+			long start_offset, long total_length, ArrayList<Host> hosts,
+			IHostPortResolver hostResolver) {
+		IncomingDownload[] subTransfers = new IncomingDownload[Math.min(
+				MAX_DOWNLOADS, num)];
 		long segmentSize = total_length / num;
 		long length = segmentSize;
 		long offset = start_offset;
 		java.io.File tmpFile;
 		int currentHost = 0;
-		for(int i = 0; i < num; i++) {
-			if(i == num - 1)  //last segment may be bigger than others
+		for (int i = 0; i < num; i++) {
+			if (i == num - 1) // last segment may be bigger than others
 				length = total_length - ((num - 1) * segmentSize);
-			
-			if(currentHost >= hosts.size())
+
+			if (currentHost >= hosts.size())
 				currentHost = 0;
-			
-			//allocate file and host
+
+			// allocate file and host
 			tmpFile = new File(hash + "-" + num + ".tmp");
 			Host host = hosts.get(currentHost++);
-			
-			//creat sub-Transfer
-			subTransfers[i] = new IncomingDownload(new GetFileMessage(hash, offset, length), host, tmpFile, hostResolver);
+
+			// creat sub-Transfer
+			subTransfers[i] = new IncomingDownload(new GetFileMessage(hash,
+					offset, length), host, tmpFile, hostResolver);
+			subTransfers[i].DisableIntegrityCheck();
 		}
 		return subTransfers;
 	}
@@ -70,7 +84,7 @@ public class MultisourceDownload extends Transfer {
 	@Override
 	public void cancel() {
 		watchDog.cancel();
-		for(Transfer t : subTransfers)
+		for (ITransferProgress t : subTransfers)
 			t.cancel();
 		transferState = TransferStatus.Canceled;
 	}
@@ -78,38 +92,89 @@ public class MultisourceDownload extends Transfer {
 	@Override
 	public void reset() {
 		watchDog.cancel();
-		for(Transfer t : subTransfers)
+		for (ITransferProgress t : subTransfers)
 			t.reset();
 		transferState = TransferStatus.Initialized;
 	}
 
 	@Override
 	public void resumeTransfer() {
-		for(Transfer t : subTransfers)
-			if(t.getStatus() == TransferStatus.TemporaryNotAvailable || 
-			t.getStatus() == TransferStatus.ConnectionFailed ||
-			t.getStatus() == TransferStatus.LostConnection)
+		for (ITransferProgress t : subTransfers)
+			if (t.getStatus() == TransferStatus.TemporaryNotAvailable
+					|| t.getStatus() == TransferStatus.ConnectionFailed
+					|| t.getStatus() == TransferStatus.LostConnection)
 				t.resumeTransfer();
 		scheduleWatchDog();
 	}
 
 	@Override
 	public void start() {
-		for(Transfer t : subTransfers) {
-			if(t.getStatus() != TransferStatus.Initialized)
-				throw new IllegalStateException("can't start because a sub-transfer is not initialized");
+		for (ITransferProgress t : subTransfers) {
+			if (t.getStatus() != TransferStatus.Initialized)
+				throw new IllegalStateException(
+						"can't start because a sub-transfer is not initialized");
 			t.start();
 		}
 		scheduleWatchDog();
 	}
-	
-	private  void scheduleWatchDog(){
+
+	private void scheduleWatchDog() {
 		watchDog.scheduleAtFixedRate(new TimerTask() {
 			@Override
 			public void run() {
-				//TODO continue
+				boolean allTransferFinished = true;
+				for (IncomingDownload t : subTransfers) {
+					if (t.getStatus() != TransferStatus.Finished)
+						allTransferFinished = false;
+				}
+
+				if (allTransferFinished)
+					assembleParts();
 			}
 		}, 5000, 500);
+	}
+
+	private void assembleParts() {
+		transferState = TransferStatus.AssembleParts;
+		
+		try {
+			FileWriter writer = new FileWriter(targetFile);
+			char[] buffer = new char[1024 * 1024];
+			for (IncomingDownload t : subTransfers) {
+				FileReader reader = new FileReader(t.getTargetFile());
+				int bytesRead = -1;
+				while (-1 != (bytesRead = reader.read(buffer, 0, buffer.length))) {
+					writer.write(buffer, 0, bytesRead); //TODO build hash while copy
+				}
+				reader.close();
+			}
+			writer.close();
+			checkIntegrity();
+		} catch (IOException e) {
+			logger.warning("could not assemble file: " + e.getMessage());
+			transferState = TransferStatus.LocalIOError;
+		}
+	}
+	
+	private void checkIntegrity() {
+		transferState = TransferStatus.CheckingHash;
+		if(checkIntegrity) {
+			try {
+				if(HashFuncWrapper.hash(targetFile.getAbsolutePath()) != hash) {
+					transferState = TransferStatus.InvalidContent;
+					return;
+				}
+			} catch (IOException e) {
+				logger.warning("Could not compute hash: " + e.getMessage());
+				transferState = TransferStatus.InvalidContent;
+				return;
+			}
+		}
+		transferState = TransferStatus.Finished;
+	}
+	
+	public void DisableIntegrityCheck() {
+		checkIntegrity = false;
 	}
 
 	@Override
